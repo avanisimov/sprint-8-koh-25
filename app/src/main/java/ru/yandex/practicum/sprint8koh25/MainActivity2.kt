d20049b294d25df9a31f7629f72a1c82fed323b6
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2024-12-26 12:08:09 +0300

    Sprint 8

--- app/src/main/java/ru/yandex/practicum/sprint8koh25/MainActivity2.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh25/MainActivity2.kt
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.sprint8koh25
 
-import android.app.ComponentCaller
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -13,59 +11,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.setPadding
-import java.util.Date
 
-class MainActivity : AppCompatActivity() {
-
-    var a = 5
+class MainActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val container = LinearLayout(this)
-        container.addView(
-            ImageView(this).apply {
-                setPadding(16)
 
-            }
-        )
+        intent.getStringExtra("key")
 
-        findViewById<Button>(R.id.btn).setOnClickListener {
-            startActivity(
-                Intent(this, MainActivity2::class.java).apply {
-                    putExtra("key", "value")
-                }
-            )
-        }
-
-        val f = (1..1_000_000).map {
-            val date = Date()
-            date.time
-        }
-
-        Log.d("SPRINT_8", "$this onCreate ${f.hashCode()}")
+        Log.d("SPRINT_8", "$this onCreate")
 
 
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
     }
 
     override fun onStart() {
